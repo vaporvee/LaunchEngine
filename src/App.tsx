cfa4a57335373c14b3537158c597e3376d295a28
vaporvee <yannik@vaporvee.com>
2024-06-13 23:37:35 +0200

    turns out you dont need open()

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { invoke } from '@tauri-apps/api'
-import { open } from '@tauri-apps/api/shell';
 import { writeTextFile, BaseDirectory, createDir, exists } from '@tauri-apps/api/fs'
 
-function createFile(){
+function createFile() {
   const fileName = "example.txt";
-          writeTextFile(fileName, "Initial content", { dir: BaseDirectory.AppData })
-            .then(() => {
-                  console.log(`Wrote to file: ${fileName}`);
-              })
-            .catch((error) => {
-                  console.error("Failed to write to file:", error);
-              });
+  writeTextFile(fileName, "Initial content", { dir: BaseDirectory.AppData })
+    .then(() => {
+      console.log(`Wrote to file: ${fileName}`);
+    })
+    .catch((error) => {
+      console.error("Failed to write to file:", error);
+    });
 }
 
 function App() {
   const [message, setMessage] = useState("")
 
-  exists(``, { dir: BaseDirectory.AppData })
-  .then((exists) => {
-    if (!exists){
-      createDir('', {dir: BaseDirectory.AppConfig}).then(() => createFile());
-    } else {
-      createFile()
-    }
-  })
+  exists("", { dir: BaseDirectory.AppData })
+    .then((exists) => {
+      if (!exists) {
+        createDir("", { dir: BaseDirectory.AppConfig }).then(() => createFile());
+      } else {
+        createFile()
+      }
+    })
 
-    // Example usage of invoke
-    invoke('greet', { name: 'World' })
-  .then((response) => {
-        setMessage(response as string);
+  // Example usage of invoke
+  invoke('greet', { name: 'World' })
+    .then((response) => {
+      setMessage(response as string);
     });
 
   return (
@@ -47,9 +46,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => open("https://vaporvee.com")}>
-          Open external
-        </button>
+        <a href="https://vaporvee.com" target="_blank">
+          <button>
+            Open external
+          </button>
+        </a>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
