e5a8a47ded2e9871718dba48531d16bfdb205ead
vaporvee <yannik@vaporvee.com>
2024-06-16 23:19:14 +0200

    removed template stuff, added folder selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { invoke } from '@tauri-apps/api'
+import { open } from '@tauri-apps/api/dialog';
 import { writeTextFile, BaseDirectory, createDir, exists } from '@tauri-apps/api/fs'
 
 function createFile() {
@@ -33,30 +31,16 @@ function App() {
     .then((response) => {
       setMessage(response as string);
     });
-
+  const [directory, setDirectory] = useState("")
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <a href="https://vaporvee.com" target="_blank">
-          <button>
-            Open external
-          </button>
-        </a>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      {message}
-    </>
+    <div className='center'>
+      <p>{message}</p>
+      <input value={directory} onChange={(e) => {setDirectory(e.target.value);}} />
+      <button onClick={async() => {
+       const folder = await open({directory: true})
+       if (folder != null) setDirectory(folder as string)
+      }}>Select folder</button>
+    </div>
   )
 }
 
